ddc8ef2096c5cdfa2af7832f115eacb0ab7cbb5b
joseettan <josef410@gmail.com>
2021-07-22 16:58:29 +0530

    Updates

---
+++ precache-manifest.1885e9c8f6e7ff4c651f8b524b239efc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f477124dfc79d64b5e0fb8dfb507eb87",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "898573a02a4364e31f07",
+    "url": "/home/static/css/main.19ae4401.chunk.css"
+  },
+  {
+    "revision": "8542d178e8325d3a04c0",
+    "url": "/home/static/js/2.b22dbfe4.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/home/static/js/2.b22dbfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "898573a02a4364e31f07",
+    "url": "/home/static/js/main.f8f50798.chunk.js"
+  },
+  {
+    "revision": "473e51da6ab1e333a3f8",
+    "url": "/home/static/js/runtime-main.2b69d4cf.js"
+  },
+  {
+    "revision": "10b99bd52be56719df5fab2311879909",
+    "url": "/home/static/media/jose2-min.10b99bd5.png"
+  },
+  {
+    "revision": "39a8d7341a68f273383070a873bf534b",
+    "url": "/home/static/media/resume.39a8d734.pdf"
+  }
+]);
